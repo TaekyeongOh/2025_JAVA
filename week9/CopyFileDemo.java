6a4029888eb650f003e1fce273d5a76e720a7862
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2025-05-15 15:56:56 +0900

    week9

---
+++ week9/CopyFileDemo.java
@@ -0,0 +1,21 @@
+package week11;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFileDemo {
+	public static void main(String[] args) {
+		String input = "C:\\Temp\\org.txt";
+		String output = "C:\\Temp\\dup.txt";
+
+		try (FileInputStream fis = new FileInputStream(input); FileOutputStream fos = new FileOutputStream(output)) {
+			int c;
+
+			while ((c = fis.read()) != -1)
+				fos.write(c);
+		} catch (IOException e) {
+
+		}
+	}
+}
---
+++ week9/CopyFileDemo2.java
@@ -0,0 +1,20 @@
+package week11;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFileDemo2 {
+	public static void main(String[] args) {
+		String input = "C:\\Temp\\org.txt";
+		String output = "C:\\Temp\\dup.txt";
+
+		try (FileReader fr = new FileReader(input); FileWriter fw = new FileWriter(output)) {
+			int c;
+
+			while ((c = fr.read()) != -1)
+				fw.write(c);
+		} catch (IOException e) {
+		}
+	}
+}
